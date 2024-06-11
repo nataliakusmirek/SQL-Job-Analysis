934caee914eb7bdd4b221e21fdf7b4453f0df22c
Natalia Kusmirek <nekusmirek@wpi.edu>
2024-06-11 15:20:03 -0400

    P5

--- project_sql/5.sql
+++ project_sql/5.sql
@@ -39,6 +39,8 @@ SELECT
 FROM
     skills_demand
 INNER JOIN average_salary ON skills_demand.skill_id = average_salary.skill_id
+WHERE
+    demand_count > 10
 ORDER BY
     demand_count DESC,
     avg_salary DESC
